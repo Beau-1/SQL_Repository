4539614f26eaba6f6c81fa29a2a4ac5d2d3648d2
Beau <jamesbuchanan0528@gmail.com>
2023-01-17 13:45:48 -0600

    Exercise 1 complete

--- SQLExercise1.sql
+++ SQLExercise1.sql
@@ -13,14 +13,14 @@ select * from bestbuy.employees where middleinitial is null;
 -- find distinct product prices
 select distinct price from products; 
 -- find all employees whose first name starts with the letter ‘j’
-select * from employees where firstname like 'j%'
+select * from employees where firstname like 'j%';
 -- find all Macbooks
-
+select * from products where name like 'mackbook%';
 -- find all products that are on sale
-
+select * from products where OnSale = 1;
 -- find the average price of all products
-
+select AVG(price) from products;
 -- find all Geek Squad employees who don't have a middle initial
-
+select * from employees where title like '%geeksquad%' and middleinitial is null;
 -- find all products from the products table whose stock level is in the range -- of 500 to 1200. Order by Price from least to greatest. Hint: Use the between keyword
-
+select * from products where stocklevel between 500 and 1200 order by price asc;
